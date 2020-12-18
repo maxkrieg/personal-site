cf4978c37824177aaef830cfe12ebd0be5096d2e
Max Krieg <maxwell.krieg@gmail.com>
2020-12-18 09:51:28 -0700

    remove container padding

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <Container fluid>
+      <Container fluid style={{ padding: 0 }}>
         <Row>
           <Col>
             <Image src={banner} fluid />
