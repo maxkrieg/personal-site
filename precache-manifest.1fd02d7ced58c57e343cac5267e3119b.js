5cda4e2e707b90c6e494aaa35d7f4fd16db92765
maxkrieg <maxwell.krieg@gmail.com>
2022-04-03 15:15:00 -0600

    Updates

---
+++ precache-manifest.1fd02d7ced58c57e343cac5267e3119b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2059e083871f7db80b4d7d985291d7c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a4430a5a3211d340ac0",
+    "url": "/static/css/main.a2030beb.chunk.css"
+  },
+  {
+    "revision": "9556748487b571be86ef",
+    "url": "/static/js/2.9081bf2f.chunk.js"
+  },
+  {
+    "revision": "2597ef821004603d17ea37bfd5dfeb97",
+    "url": "/static/js/2.9081bf2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a4430a5a3211d340ac0",
+    "url": "/static/js/main.ff4fd6e0.chunk.js"
+  },
+  {
+    "revision": "766df7dbb5392e1314ed",
+    "url": "/static/js/runtime-main.9c9c41ef.js"
+  },
+  {
+    "revision": "c1ccb9c91d698b17c6014a8b7b909133",
+    "url": "/static/media/max-rowan.c1ccb9c9.jpg"
+  },
+  {
+    "revision": "df57d4af7c0159c8e6c84fef3d63ebef",
+    "url": "/static/media/zion-background.df57d4af.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cbe62c777fe395f7238ff243937025a0.js"
+  "/precache-manifest.1fd02d7ced58c57e343cac5267e3119b.js"
 );
 
 self.addEventListener('message', (event) => {
